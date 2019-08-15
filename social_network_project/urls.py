ed43646874f10c7e890683a96b11404e1712fa49
maxim.volkov <maxim.volkov@buidl.ru>
2019-08-15 16:00:01 +0300

    Fix urlpatterns

--- social_network_project/urls.py
+++ social_network_project/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'^tinymce/', include('tinymce.urls')),
-    path(r'^auth/', include('django.contrib.auth.urls')),
+    path('tinymce/', include('tinymce.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
